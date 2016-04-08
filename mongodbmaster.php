b5c703de47787b690d141afeb0fa98744571afd0
Riffmeister <niri5023@colorado.edu>
2016-04-08 01:51:58 -0600

    Made further changes and should now be ready for javascript calls to insert into separate collections

--- mongodbmaster.php
+++ mongodbmaster.php
@@ -4,4 +4,30 @@ $connection = new MongoClient();
 
 $db = $connection->WhatsPoppin;
 
+function insertTwitter($text, $id) {
+	$collection = $db->Twitter
+	$doc = array(
+		"text" => $text,
+		"id" => $id
+	);
+	$collection->insert( $doc );
+}
+
+function insertYelp($name, $id) {
+        $collection = $db->Yelp
+        $doc = array(
+                "name" => $name,
+                "id" => $id
+        );
+	$collection->insert( $doc );
+}
+
+function insertInstagram($Q) {
+        $collection = $db->instagram
+        $doc = array(
+                "tag" => $Q
+        );
+	$collection->insert( $doc );
+}
+
 ?>
