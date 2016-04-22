ba18324cfde622aade1746c726bb14023149cc23
Andrew Kiproff <anki7203@colorado.edu>
2016-04-22 16:28:34 -0600

    upload of app
    r.1

---
+++ app.php
@@ -0,0 +1,39 @@
+<?php
+
+function insertOutput($poppinlocale) {
+        $connection = new MongoClient(); //initiate mongo
+        $db = $connection->WhatsPoppin; //use WhatsPoppin
+        $collection = $db->result;
+	
+        $collection->update(
+		array("poppinlocale" => $poppinlocale),
+		array('$inc' => array("hits" => 1)),
+		array("dayofweek" => date("l")),
+		array("multiple" => true)
+		);
+
+		$cursor = $collection->find();
+
+		foreach ($cursor as $document) {
+		    echo $document["poppinlocale"] . "\n";
+		}
+}
+
+
+function insertInput($text, $poppinlocale) {
+	$connection = new MongoClient(); //initiate mongo
+	$db = $connection->WhatsPoppin; //use WhatsPoppin
+	$collection = $db->introdata;
+	$doc = array(
+		"message" => $text,
+		"id" => $poppinlocale,
+		"dayofweek" => date("l")
+	);
+	$collection->insert( $doc );
+	insertOutput($poppinlocale);
+}
+
+
+insertOutput('TestPlace1');
+
+?>
